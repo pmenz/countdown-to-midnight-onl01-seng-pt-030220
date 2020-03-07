78fa30c7d66a8324f1fc8be01a6e47417d892107
Pablo Menz <p.menz.r@gmail.com>
2020-03-07 20:03:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,21 +2,20 @@
 
 
 def countdown(n=10)
-    while n > 0
+  while n > 0
     puts "#{n} SECOND(S)!"
     n -= 1
     end
-    puts"HAPPY NEW YEAR!"
-  end
+ puts"HAPPY NEW YEAR!"
+end
   
 
-def countdown_with_sleep(number)
-  number=12
+def countdown_with_sleep(number=12)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
     sleep(1)
-    end
-    puts"HAPPY NEW YEAR!"
   end
+ puts"HAPPY NEW YEAR!"
+end
   
