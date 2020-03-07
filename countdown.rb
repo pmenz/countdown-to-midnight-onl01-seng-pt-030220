326dbfb2ae8e0e2d91673c18e9b3fd4db50c6e3e
Pablo Menz <p.menz.r@gmail.com>
2020-03-07 20:05:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,6 @@
 #write your code here
 
 
-def countdown(n=10)
-  while n > 0
-    puts "#{n} SECOND(S)!"
-    n -= 1
-    end
- puts"HAPPY NEW YEAR!"
-end
   
 
 def countdown_with_sleep(number=12)
